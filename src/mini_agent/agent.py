f85ec86601b367e765450fc8a7bc7ce1615168cc
Minh Le <ducminhldm@gmail.com>
2025-12-20 23:18:17 +0700

    wip agent loop

--- src/mini_agent/agent.py
+++ src/mini_agent/agent.py
@@ -1,7 +1,22 @@
-"""Core Agent class."""
+"""Tool-using agent."""
 from __future__ import annotations
+from typing import Any
 
 
-class _Placeholder:
-    """Skeleton for agent. Will be fleshed out."""
-    pass
+class Agent:
+    """Tool-using agent."""
+
+    def __init__(self, **config: Any) -> None:
+        self.config = config
+
+    def run(self, *args, **kwargs):
+        """Run a single turn."""
+        raise NotImplementedError
+
+    def step(self, *args, **kwargs):
+        """One agent step."""
+        raise NotImplementedError
+
+    def _select_tool(self, *args, **kwargs):
+        """Pick a tool for this step."""
+        raise NotImplementedError
