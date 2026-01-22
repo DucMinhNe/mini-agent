82a94674334dfcc5e6e91728fcb5500b1348bbff
Minh Le <ducminhldm@gmail.com>
2026-01-22 14:18:31 +0700

    add fastapi server

---
+++ src/mini_agent/server/app.py
@@ -0,0 +1,12 @@
+"""FastAPI app."""
+from __future__ import annotations
+
+
+def create_app():
+    """Create FastAPI instance."""
+    raise NotImplementedError
+
+
+def chat_endpoint(req):
+    """Handle POST /chat."""
+    raise NotImplementedError
